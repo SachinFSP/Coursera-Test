9e2f6aa9d49fdbef3b9db914f9fd249d44915257
Sachin Hiriyanna <sachin.h94@hotmail.com>
2022-06-21 15:18:25 -0700

    done

---
+++ module_1_solution/app.js
@@ -0,0 +1,21 @@
+(function () {
+  "use strict";
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+
+  function LunchCheckController($scope) {
+    $scope.itemsCsv = "";
+    $scope.result = "";
+
+    $scope.checkLunch = function () {
+      var items = $scope.itemsCsv.split(",").filter(function (item) {
+        return item !== "";
+      });
+
+      if (items.length <= 0) $scope.result = "Please enter data first";
+      else if (items.length <= 3) $scope.result = "Enjoy!";
+      else $scope.result = "Too much!";
+    };
+  }
+})();
